07ca6bfa9f78196712dd58161f8516ea2dc480ad
MariusSam <34354282+MariusSam@users.noreply.github.com>
2019-06-15 18:29:48 +0300

    Created database connection

---
+++ RestWebService/RestWebService/Database/DbConnection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Web;
+
+namespace RestWebService.Database
+{
+    public abstract class DbConnection
+    {
+
+        protected SQLiteConnection Con { get; set; }
+
+        protected void Connect()
+        {
+            Con = new SQLiteConnection(@"Data Source=database.db;");
+            if (Con.State != System.Data.ConnectionState.Open)
+            {
+                Con.Open();
+            }
+        }
+
+        protected void Disconnect()
+        {
+            if (Con != null)
+            {
+                if (Con.State == System.Data.ConnectionState.Open)
+                    Con.Close();
+            }
+        }
+
+    }
+}
