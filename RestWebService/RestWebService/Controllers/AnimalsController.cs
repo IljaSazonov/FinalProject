af51660462e2767fb7ea7b9903562acff3b1bd2f
MariusSam <34354282+MariusSam@users.noreply.github.com>
2019-06-16 02:29:01 +0300

    + Post method / Updated get Method

--- RestWebService/RestWebService/Controllers/AnimalsController.cs
+++ RestWebService/RestWebService/Controllers/AnimalsController.cs
@@ -18,12 +18,42 @@ namespace RestWebService.Controllers
             }
         }
 
-        public Animal Get(int id)
+        public HttpResponseMessage Get(int id)
         {
             using (AnimalsDBEntities entities = new AnimalsDBEntities())
             {
-                return entities.Animals.FirstOrDefault(e => e.ID ==id);
+                var entity = entities.Animals.FirstOrDefault(e => e.ID ==id);
                
+                if(entity != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, entity);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Animal with ID = " + id.ToString() + " not found");
+                }
+            }
+        }
+
+        public HttpResponseMessage Post([FromBody] Animal animal)
+        {
+            try
+            {
+
+                using (AnimalsDBEntities entities = new AnimalsDBEntities())
+                {
+                    entities.Animals.Add(animal);
+                    entities.SaveChanges();
+
+                    var message = Request.CreateResponse(HttpStatusCode.Created, animal);
+                    message.Headers.Location = new Uri(Request.RequestUri + animal.ID.ToString());
+                    return message;
+                }
+
+            }
+            catch(Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, e);
             }
         }
     }
