777c217d565831f524b2c79414d4ee0fe02c7220
MariusSam <34354282+MariusSam@users.noreply.github.com>
2019-06-15 22:48:39 +0300

    DB Updated

---
+++ RestWebService/RestWebService/Controllers/AnimalsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DataAccess;
+
+namespace RestWebService.Controllers
+{
+    public class AnimalsController : ApiController
+    {
+        public IEnumerable<Animal> Get()
+        {
+            using(AnimalsDBEntities entities = new AnimalsDBEntities())
+            {
+                return entities.Animals.ToList();
+            }
+        }
+
+        public Animal Get(int id)
+        {
+            using (AnimalsDBEntities entities = new AnimalsDBEntities())
+            {
+                return entities.Animals.FirstOrDefault(e => e.ID ==id);
+               
+            }
+        }
+    }
+}
