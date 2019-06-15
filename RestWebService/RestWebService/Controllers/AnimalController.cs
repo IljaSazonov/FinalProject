08a250c9becc593101d2c709511f5e8f957e2c3e
IljaSazonov <34349810+IljaSazonov@users.noreply.github.com>
2019-06-15 16:14:35 +0300

    Added animal controler

---
+++ RestWebService/RestWebService/Controllers/AnimalController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RestWebService.Controllers
+{
+    public class AnimalController : ApiController
+    {
+        // GET: api/Animal
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Animal/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Animal
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Animal/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Animal/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
