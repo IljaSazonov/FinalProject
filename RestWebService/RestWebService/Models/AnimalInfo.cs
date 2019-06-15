11a377b3e8e10c90a5fc99000ab564fe6b1e4e16
IljaSazonov <34349810+IljaSazonov@users.noreply.github.com>
2019-06-15 16:27:21 +0300

    Added animal information

---
+++ RestWebService/RestWebService/Models/AnimalInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace RestWebService.Models
+{
+    [DataContract]
+    public class AnimalInfo
+    {
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "type")]
+        public string Type { get; set; }
+
+        [DataMember(Name = "age")]
+        public int Age { get; set; }
+
+        [DataMember(Name = "gender")]
+        public string Gender { get; set; }
+
+        [DataMember(Name = "sterilization")]
+        public string Sterilization { get; set; }
+
+          
+    }
+}
