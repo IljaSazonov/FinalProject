f3609b37499d3f620bf3dcdeb4409d6da8d2331b
IljaSazonov <34349810+IljaSazonov@users.noreply.github.com>
2019-06-15 18:28:51 +0300

    added xml comments

--- RestWebService/RestWebService/Controllers/AnimalController.cs
+++ RestWebService/RestWebService/Controllers/AnimalController.cs
@@ -8,6 +8,9 @@ using System.Web.Http;
 
 namespace RestWebService.Controllers
 {
+    /// <summary>
+    /// Information about all animals.
+    /// </summary>
     public class AnimalController : ApiController
     {
         List<AnimalInfo> animalList = new List<AnimalInfo>();
@@ -17,25 +20,42 @@ namespace RestWebService.Controllers
             animalList.Add(new AnimalInfo { Id = 1, Age = 5, Gender = "Male", Name = "Doggo", Sterilization = "asd", Type = "Dog" });
             animalList.Add(new AnimalInfo { Id = 2, Age = 3, Gender = "Female", Name = "Cattie", Sterilization = "asd", Type = "Cat" });
         }
-
+        
+        /// <summary>
+        /// List containing all animals.
+        /// </summary>
+        /// <returns> animal list </returns>
         // GET: api/Animal
         public List<AnimalInfo> Get()
         {
             return animalList;
         }
 
+        /// <summary>
+        /// Finds animal by his id.
+        /// </summary>
+        /// <param name="id">Id of an animal</param>
+        /// <returns>Animal with an specific id</returns>
         // GET: api/Animal/5
         public AnimalInfo Get(int id)
         {
             return animalList.Where(x => x.Id == id).FirstOrDefault();
         }
 
+        /// <summary>
+        /// Add new animal entry to list.
+        /// </summary>
+        /// <param name="val">Animal list</param>
         // POST: api/People
         public void Post(AnimalInfo val)
         {
             animalList.Add(val);
         }
 
+        /// <summary>
+        /// Deletes animal from the list.
+        /// </summary>
+        /// <param name="id">Id of an animal</param>
         // DELETE: api/Animal/5
         public void Delete(int id)
         {
--- RestWebService/RestWebService/Models/AnimalInfo.cs
+++ RestWebService/RestWebService/Models/AnimalInfo.cs
@@ -6,24 +6,45 @@ using System.Web;
 
 namespace RestWebService.Models
 {
+    /// <summary>
+    /// Represents animal information.
+    /// </summary>
     [DataContract]
     public class AnimalInfo
     {
+        /// <summary>
+        /// Id from sql database.
+        /// </summary>
         [DataMember(Name = "id")]
         public int Id { get; set; }
 
+        /// <summary>
+        /// Animal's name.
+        /// </summary>
         [DataMember(Name = "name")]
         public string Name { get; set; }
 
+        /// <summary>
+        /// Animal's type.
+        /// </summary>
         [DataMember(Name = "type")]
         public string Type { get; set; }
 
+        /// <summary>
+        /// Animal's age.
+        /// </summary>
         [DataMember(Name = "age")]
         public int Age { get; set; }
 
+        /// <summary>
+        /// Animals' gender.
+        /// </summary>
         [DataMember(Name = "gender")]
         public string Gender { get; set; }
 
+        /// <summary>
+        /// Sterilization.
+        /// </summary>
         [DataMember(Name = "sterilization")]
         public string Sterilization { get; set; }
 
